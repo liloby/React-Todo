92e3f3889958789a21a930dbbf936e72d118b15f
Oby <obyli214@gmail.com>
2023-03-14 05:58:34 -0700

    add mobile responsive title

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 import { useTheme } from "@mui/material/styles";
+import PhoneIphoneIcon from '@mui/icons-material/PhoneIphone';
 import {
   Box,
   Container,
@@ -21,10 +22,11 @@ function App() {
         variant={isMobile ? "h5" : "h1"}
         className="app-name"
       >
-        <span style={{fontSize: '4rem'}}>React</span>{" "}
+        <span style={{fontSize: isMobile ? '3rem' : '4rem'}}>React</span>{" "}
         <span style={{ transform: 'rotate(-20deg)', display: 'inline-block', color: '#008a10' }}>To</span>{" "}
+        {isMobile ? <PhoneIphoneIcon sx={{transform: 'translate(0px, -10px)', margin: '-.2rem .2rem -.2rem -.2rem', fontSize: "1rem"}} /> : ''}
         <span style={{ transform: 'rotate(20deg)', display: 'inline-block', color: '#ff0000' }}>Do</span>{" "}
-        <span style={{fontSize: '4rem'}}>App</span>
+        <span style={{fontSize: isMobile ? '3rem' : '4rem'}}>App</span>
       </Typography>
       <TodoList isMobile={isMobile}/>
     </Container>
